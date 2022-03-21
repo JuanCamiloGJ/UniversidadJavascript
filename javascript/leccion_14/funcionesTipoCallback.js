dc05f46faa49bda3e2fb636d3791c9258a000146
JuanCamiloGJ <juankamilogarciajimenez@gmail.com>
2022-03-21 14:31:10 -0500

    Funciones de tipo callback y su uso con la funcion setInterval()

--- javascript/leccion_14/funcionesTipoCallback.js
+++ javascript/leccion_14/funcionesTipoCallback.js
@@ -1,37 +1,6 @@
-function miFunction1(){
-    console.log("funcion1");
+let reloj = () =>{
+    let fecha = new Date();
+    console.log(`La hora es: ${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}`);
 }
 
-function miFunction2(){
-    console.log("funcion2");
-}
-
-miFunction1();
-miFunction2();
-
-
-//funciones de tipo callback
-function imprimir(mensaje){
-    console.log(mensaje);
-}
-
-function sumar(op1, op2, funcionCallback){
-    let res = op1 + op2;
-    funcionCallback(`Resultado: ${res}`);
-
-}
-
-sumar(5,3, imprimir);
-
-
-//Llamadas asíncronas con uso setTimeout
-
-function miFuncionCallback(){
-    console.log("Saludo asincrono despues de 3 seg");
-}
-
-setTimeout(miFuncionCallback, 3000); // se ejecuta despues de 3seg
-
-setTimeout(function(){console.log("saludo asincorno 2 ")}, 4000);
-
-setTimeout( () => console.log("saludo asincrono 3"), 1000); 
+setInterval(reloj, 1000);
